b89e083c441c35d8f178c08a6c604d70dc6c9d50
Gema Parreño <gema.parreno.piqueras@gmail.com>
2023-07-20 19:05:09 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-inputs = tokenizer(["Margaret is outstanding, well known as a"], return_tensors="pt")
-summary_ids = model.generate(inputs["input_ids"], max_new_tokens=3)
+inputs = tokenizer(["The cake is a"], return_tensors="pt")
+summary_ids = model.generate(inputs["input_ids"], max_new_tokens=17)
 
 print(tokenizer.batch_decode(summary_ids, skip_special_tokens=True)[0])
 
@@ -17,9 +17,9 @@ def get_tokens_as_list(word_list):
         tokens_list.append(tokenized_word)
     return tokens_list
 
-word_list = "great writer"
+word_list = " very good example of a cake that is made with a very good quality of ingredients."
 bad_words_ids = get_tokens_as_list(word_list=word_list)
 
-badwords_ids2 = model.generate(inputs["input_ids"], max_new_tokens=3, bad_words_ids=bad_words_ids)
+badwords_ids2 = model.generate(inputs["input_ids"], max_new_tokens=17, bad_words_ids=bad_words_ids, eos_token_id=tokenizer_with_prefix_space.eos_token_id)
 
 print(tokenizer.batch_decode(badwords_ids2, skip_special_tokens=True)[0])
