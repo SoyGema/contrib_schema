75559fd441b9d50d0def304d98e4fca2c862b62a
Gema Parreño <gema.parreno.piqueras@gmail.com>
2023-07-21 14:58:44 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-inputs = tokenizer(["The cake is a"], return_tensors="pt")
-summary_ids = model.generate(inputs["input_ids"], max_new_tokens=12 , pad_token_id=tokenizer.eos_token_id)
+inputs = tokenizer(["In a word, the cake is a"], return_tensors="pt")
+summary_ids = model.generate(inputs["input_ids"], max_new_tokens=5 , pad_token_id=tokenizer.eos_token_id)
 
 print(tokenizer.batch_decode(summary_ids, skip_special_tokens=True)[0])
 
@@ -17,12 +17,12 @@ def get_tokens_as_list(word_list):
         tokens_list.append(tokenized_word)
     return tokens_list
 
-word_list = "but it's not a problem."
+word_list = "mess"
 bad_words_ids = get_tokens_as_list(word_list=word_list)
 
-badwords_ids2 = model.generate(inputs["input_ids"], max_new_tokens=17, bad_words_ids=bad_words_ids, eos_token_id=tokenizer_with_prefix_space.eos_token_id)
+badwords_ids2 = model.generate(inputs["input_ids"], max_new_tokens=5, bad_words_ids=bad_words_ids, eos_token_id=tokenizer_with_prefix_space.eos_token_id)
 
 print(tokenizer.batch_decode(badwords_ids2, skip_special_tokens=True)[0])
 
-biased_ids = model.generate(inputs["input_ids"], max_new_tokens=4, num_beams=5, bad_words_ids=bad_words_ids)
+biased_ids = model.generate(inputs["input_ids"], max_new_tokens=5, num_beams=5, bad_words_ids=bad_words_ids)
 print(tokenizer.batch_decode(biased_ids, skip_special_tokens=True)[0])
